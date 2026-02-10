0019f5c786f0834230dd86c10c96a18a0bb06a23
ppxb <317842449@qq.com>
2026-02-10 13:34:42 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,6 @@
 import antfu from '@antfu/eslint-config'
-import oxlint from 'eslint-plugin-oxlint'
 
 export default antfu({
   unocss: true,
-  formatters: {
-    css: true,
-    html: true,
-  },
-}, oxlint.configs['flat/recommended'])
+  formatters: true,
+})
